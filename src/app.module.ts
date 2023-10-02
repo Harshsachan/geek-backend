e28b66dcb26b1fb426f2ab9340048009375fb4b7
Harshsachan <harshit.26sachan@gmail.com>
2023-10-02 14:19:35 +0530

    signup-module -P

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SignUpModule } from './sign-up/sign-up.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type:'mongodb',
+    url:process.env.MONGODB_URI,
+    synchronize:false,
+    useUnifiedTopology:true,
+    entities:[]
+  }),SignUpModule],
   controllers: [AppController],
   providers: [AppService],
 })
